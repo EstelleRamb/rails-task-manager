268f94fa9f70a439103044a5ec0addee770e52a9
estelle rambur <estelle.rambur@gmail.com>
2022-11-22 13:45:41 -0500

    As a user I can remove a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,9 +28,17 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
+    @task.destroy
+    redirect_to taskss_path, status: :see_other
   end
 end
